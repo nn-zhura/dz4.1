7c4ab4aa443b006a15d4475e296214c718253d04
nika <jarodos572@finxmail.net>
2021-12-21 10:15:39 +0300

    домашка 4

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {   
+        /// <summary>
+        /// Метод, для поиска большого числа в массиве
+        /// </summary>
+        /// <param name="mas">Массив, в котором нужно найти максимальное число</param>
+        /// <returns></returns>
+        public static int MaxInt(int[] mas) //объявление метода, для нахождения макс.числа элемента в массиве
+        {//0
+            int max = mas[0];//1 //к переменной max присваивается первый элемент в массиве
+            for (int i = 0; i < mas.Length; i++)//2 //к переменной i присваивается начальное значение =0,
+            {//3
+                if (max < mas[i])//4
+                    max = mas[i];//5
+            }//6
+            return max;//7
+        }//8
+    }
+}
---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.ReferenceEquals(3, Class1.MaxInt(new int[] {1, 2, 3}));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.ReferenceEquals(1, Class1.MaxInt(new int[] {1,1,1}));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.ReferenceEquals(0, Class1.MaxInt(new int[] { }));
+        }
+        
+    }
+}
